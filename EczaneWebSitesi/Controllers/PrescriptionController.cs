2322921b2742d543c1479d5fe1f2d583ea49b2a9
Emir Görkem Özdemir <Emir Grkem zdemir@DESKTOP-5TA2SUU>
2025-03-22 21:58:20 +0300

    Proje dosyası ekle.

---
+++ EczaneWebSitesi/Controllers/PrescriptionController.cs
@@ -0,0 +1,21 @@
+﻿using EczaneWebSitesi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EczaneWebSitesi.Controllers
+{
+    public class PrescriptionController : Controller
+    {
+        EczaneDBEntities db = new EczaneDBEntities();
+
+        [HttpGet]
+        public ActionResult ListPrescriptions()
+        {
+            List<Prescription> receteler = db.Prescriptions.ToList();
+            return View(receteler);
+        }
+    }
+}
