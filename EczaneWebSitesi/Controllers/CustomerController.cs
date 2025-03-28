e1a32a4537ce899a6377fe5d43482a26b5e13cb1
Emir Görkem Özdemir <Emir Grkem zdemir@DESKTOP-5TA2SUU>
2025-03-28 21:24:35 +0300

    implemented

--- EczaneWebSitesi/Controllers/CustomerController.cs
+++ EczaneWebSitesi/Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 ﻿using EczaneWebSitesi.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -26,6 +27,7 @@ namespace EczaneWebSitesi.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public ActionResult AddCustomer(Customer customer)
         {
@@ -46,10 +48,25 @@ namespace EczaneWebSitesi.Controllers
 
         public ActionResult DeleteCustomer(int id)
         {
-            var secili_recete = db.Customers.Find(id);
-            db.Customers.Remove(secili_recete);
+            var secili_musteri = db.Customers.Find(id);
+            db.Customers.Remove(secili_musteri);
             db.SaveChanges();
 
+            return RedirectToAction("ListCustomers");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateCustomer(int id)
+        {
+            Customer selected  = db.Customers.Find(id);
+            return View(selected);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCustomer(Customer musteri)
+        {
+            db.Customers.AddOrUpdate(musteri);
+            db.SaveChanges();
             return RedirectToAction("ListCustomers");
         }
     }
